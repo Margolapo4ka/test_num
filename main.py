8ab6b4afc0be2152efb98ec5ed049501e1d9c9bf
Margo <89163432226@mail.ru>
2023-02-27 14:38:22 +0300

    add user answer

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-num = int(input())
-for i in range(1, 11):
-    print(f"{i} * {num} = {i * num}")
+# num = int(input())
+for j in range(1, 11):
+    print("Таблица умножения на:", j)
+    for i in range(1, 11):
+
+        print(f"Сколько будет: {i} * {j} = ?")
+        num = int(input())
+        if i * j == num:
+            print("Правильно!!!")
+
+        else:
+            print(f"Вы ошиблись :( \n Ответ: {i * j} ")
+
+    print()
